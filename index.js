ae54e184557de635763d185a37994cc589cd50f7
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-04-14 18:46:14 -0400

    got my location back on the map

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const myMap = {
 	map: {},
 	markers: {},
 
-	buildMap(coordinates) {
+	buildMap() {
 		this.map = L.map('map', {
 			center: this.coordinates,
 			zoom: 11,
@@ -72,7 +72,7 @@ const myMap = {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(this.map);
 
-		const marker = L.marker([coords[0], coords[1]])
+		const marker = L.marker(this.coordinates)
 			.addTo(this.map)
 			.bindPopup('You are here')
 			.openPopup();
@@ -89,23 +89,23 @@ async function getCoords() {
     return [pos.coords.latitude, pos.coords.longitude]
 }	
 // get foursquare businesses
-// async function getFoursquare(business) {
-// 	const options = {
-// 		method: 'GET',
-// 		headers: {
-// 		Accept: 'application/json',
-// 		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
-// 		}
-// 	}
-// 	let limit = 5
-// 	let lat = myMap.coordinates[0]
-// 	let lon = myMap.coordinates[1]
-// 	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
-// 	let data = await response.text()
-// 	let parsedData = JSON.parse(data)
-// 	let businesses = parsedData.results
-// 	return businesses
-// }
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
+		}
+	}
+	let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
 window.onload = async () => {
     const coords = await getCoords()
 	console.log(coords)
